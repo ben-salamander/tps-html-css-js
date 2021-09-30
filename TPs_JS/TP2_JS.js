9e822ab10a9b4fe443d835b7e940267978d142a6
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-09-30 15:36:38 +0200

    mise en place de la SandBox et TP2

---
+++ TPs_JS/TP2_JS.js
@@ -0,0 +1,27 @@
+let age = 200;
+while (age != 0) {
+  age = prompt(
+    "Veuillez entrer l'age de votre enfant : \n\r Tapez juste entrer pour sortir"
+  );
+  if ((age >= 7) & (age <= 9)) {
+    alert("Votre enfant est un poussin.");
+  } else {
+    if ((age >= 10) & (age <= 11)) {
+      alert("Votre enfant est une pupille.");
+    } else {
+      if ((age >= 12) & (age <= 13)) {
+        alert("Votre enfant est un benjamin.");
+      } else {
+        if ((age >= 14) & (age <= 15)) {
+          alert("Votre enfant est un minime.");
+        } else {
+          if ((age >= 16) & (age <= 17)) {
+            alert("Votre enfant est un cadet.");
+          } else {
+            alert("Cette personne ne rentre pas dans une catégorie.");
+          }
+        }
+      }
+    }
+  }
+}
