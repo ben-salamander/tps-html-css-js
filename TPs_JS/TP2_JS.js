f68b16bc3556f0aaae3ce5dd79dd9bbc33dd91ad
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-09-30 16:20:19 +0200

    suppression de la boucle tp2_JS

--- TPs_JS/TP2_JS.js
+++ TPs_JS/TP2_JS.js
@@ -1,25 +1,22 @@
-let age = 200;
-while (age != 0) {
-  age = prompt(
-    "Veuillez entrer l'age de votre enfant : \n\r Tapez juste entrer pour sortir"
-  );
-  if ((age >= 7) & (age <= 9)) {
-    alert("Votre enfant est un poussin.");
+age = prompt(
+  "Veuillez entrer l'age de votre enfant : \n\r Tapez juste entrer pour sortir"
+);
+if ((age >= 7) & (age <= 9)) {
+  alert("Votre enfant est un poussin.");
+} else {
+  if ((age >= 10) & (age <= 11)) {
+    alert("Votre enfant est une pupille.");
   } else {
-    if ((age >= 10) & (age <= 11)) {
-      alert("Votre enfant est une pupille.");
+    if ((age >= 12) & (age <= 13)) {
+      alert("Votre enfant est un benjamin.");
     } else {
-      if ((age >= 12) & (age <= 13)) {
-        alert("Votre enfant est un benjamin.");
+      if ((age >= 14) & (age <= 15)) {
+        alert("Votre enfant est un minime.");
       } else {
-        if ((age >= 14) & (age <= 15)) {
-          alert("Votre enfant est un minime.");
+        if ((age >= 16) & (age <= 17)) {
+          alert("Votre enfant est un cadet.");
         } else {
-          if ((age >= 16) & (age <= 17)) {
-            alert("Votre enfant est un cadet.");
-          } else {
-            alert("Cette personne ne rentre pas dans une catégorie.");
-          }
+          alert("Cette personne ne rentre pas dans une catégorie.");
         }
       }
     }
