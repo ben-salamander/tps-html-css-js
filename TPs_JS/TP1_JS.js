037b6fd6179032a3e4f3221522820cf6f0583651
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-09-30 14:46:04 +0200

    Réalisation TP1_JS.js

---
+++ TPs_JS/TP1_JS.js
@@ -0,0 +1,7 @@
+let a = 20;
+let b = 22;
+const c = a + b;
+alert("Bienvenue sur la page de formulaire !");
+console.log("a = " + a.toString());
+console.log("b = " + b.toString());
+console.log("Le résultat de a + b est " + c.toString());
