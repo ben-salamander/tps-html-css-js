5f2a3089b24e03cbfe5f130c2638d4eedecfd54b
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-09-30 16:13:08 +0200

    créaton TP2 et renommage tp1

---
+++ TPs_JS/TP2.js
@@ -0,0 +1,4 @@
+let dist = prompt("Entrer la distance parcourue en mètres :");
+let temps = prompt("Entrer le temps parcouru en secondes:");
+let vitesse = dist / temps;
+alert("La vitesse était de " + vitesse + "m/s");
