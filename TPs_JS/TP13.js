cf2e18b437285fd99917bb42f4b4dbf8e9288213
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-10-01 16:46:16 +0200

    realisation tp13

---
+++ TPs_JS/TP13.js
@@ -0,0 +1,9 @@
+const tab = [5, 87, 4, 92, 12, 35, 64, 14];
+let sum = 0;
+for (let i = 0; i < tab.length; i++) {
+  sum = sum + tab[i];
+}
+
+console.log(
+  `Somme de ${tab.toString()} = ${sum}, la moyenne est de ${sum / tab.length}`
+);
