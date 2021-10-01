e86d3566c7663d98ac6366124b690d3a4bef95f9
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-10-01 10:59:05 +0200

    correction TP3

--- TPs_JS/TP3.js
+++ TPs_JS/TP3.js
@@ -1,24 +1,14 @@
-age = prompt(
-  "Veuillez entrer l'age de votre enfant : \n\r Tapez juste entrer pour sortir"
-);
-if ((age >= 7) & (age <= 9)) {
+age = prompt("Veuillez entrer l'age de votre enfant :");
+if (age >= 7 && age <= 9) {
   alert("Votre enfant est un poussin.");
+} else if (age >= 10 && age <= 11) {
+  alert("Votre enfant est une pupille.");
+} else if (age >= 12 && age <= 13) {
+  alert("Votre enfant est un benjamin.");
+} else if (age >= 14 && age <= 15) {
+  alert("Votre enfant est un minime.");
+} else if (age >= 16 && age <= 17) {
+  alert("Votre enfant est un cadet.");
 } else {
-  if ((age >= 10) & (age <= 11)) {
-    alert("Votre enfant est une pupille.");
-  } else {
-    if ((age >= 12) & (age <= 13)) {
-      alert("Votre enfant est un benjamin.");
-    } else {
-      if ((age >= 14) & (age <= 15)) {
-        alert("Votre enfant est un minime.");
-      } else {
-        if ((age >= 16) & (age <= 17)) {
-          alert("Votre enfant est un cadet.");
-        } else {
-          alert("Cette personne ne rentre pas dans une catégorie.");
-        }
-      }
-    }
-  }
+  alert("Cette personne ne rentre pas dans une catégorie.");
 }
