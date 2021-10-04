8dbb420dc14e1eb8f4491ab55f53af1094da7b93
Benjamin BOSSARD <benjamin.nbossard@gmail.com>
2021-10-04 15:38:59 +0200

    Réalisation TP21

---
+++ TPs_JS/TP21.js
@@ -0,0 +1,58 @@
+//CLASSES
+class student {
+  constructor() {
+    alert("Création d'un étudiant");
+    this.nom = prompt("Nom :");
+    this.prenom = prompt("Prénom :");
+    this.age = parseInt(prompt("Age"));
+    this.genre = prompt("Genre :");
+    this.pays = prompt("Pays");
+    this.option = prompt("Option");
+    this.isEditable =
+      prompt("Les nom, prénom et options sont-ils modifables ?") ==
+      ("Oui" || "oui" || "o")
+        ? true
+        : false;
+    this.listCours = this.newListCours();
+  }
+
+  //Fonction de construction de la liste des cours
+  newListCours() {
+    let cours = "cours";
+    let list = [];
+    while (cours != "") {
+      cours = prompt("Nouveaux cours");
+      list.push(cours);
+    }
+    list.pop();
+    return list;
+  }
+
+  // constructor(nom,prenom,age,genre,pays,option,isEditable,listCours){
+  //     this.nom = nom;
+  //     this.prenom = prenom;
+  //     this.age = age ;
+  //     this.genre = genre;
+  //     this.pays = pays;
+  //     this.option = option;
+  //     this.isEditable = isEditable;
+  //     this. listCours = listCours;
+  // }
+
+  tostring() {
+    console.log(
+      `Nom : ${this.nom}
+Prenom : ${this.prenom}
+Age : ${this.age}
+Genre : ${this.genre}
+Pays : ${this.pays}
+Option : ${this.option}
+Modiable : ${this.isEditable ? "Oui" : "Non"}
+Liste des cours : ${this.listCours}`
+    );
+  }
+}
+
+//PROGRAMME
+const etudiant = new student();
+etudiant.tostring();
